eb0b9e8ade8ef6f5899cc41352528e75d2e608cc
MaksGovor <maksgovruha@gmail.com>
2020-05-23 19:29:53 +0300

    Add linter, and lintering code

--- code.js
+++ code.js
@@ -4,32 +4,32 @@ const buildMatrix = (cols, rows) => {
   const matrix = new Array();
   let id = 1;
 
-  for (let y = 0; y < rows; y++){
+  for (let y = 0; y < rows; y++) {
     const row = new Array();
     for (let x = 0; x < cols; x++)
       row.push({
-          id: id++,
-          visible: false,
-          left: false,
-          right: false,
-          flag: false,
-          mine: false,
-          mineCount: 0,
-          poten: false,
-          x,
-          y,
+        id: id++,
+        visible: false,
+        left: false,
+        right: false,
+        flag: false,
+        mine: false,
+        mineCount: 0,
+        poten: false,
+        x,
+        y,
       });
     matrix.push(row);
   }
-  
+
   return matrix;
-}
+};
 
 
 const randomClearCell = matrix => {
   const cells = new Array();
-  for (let y = 0;y < matrix.length; y++) {
-    for (let x = 0;x < matrix[y].length; x++){
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
       const cell = matrix[y][x];
       if (!cell.mine) cells.push(cell);
     }
@@ -37,38 +37,38 @@ const randomClearCell = matrix => {
 
   const index = Math.floor(Math.random() * cells.length);
   return cells[index];
-}
+};
 
 const randomMine = matrix => {
   const cell = randomClearCell(matrix);
   if (cell) cell.mine = true;
-  const cells = aroundCells (matrix, cell.x, cell.y);
-  
-  for (const cell of cells) cell.mineCount++;
-}
+  const cells = aroundCells(matrix, cell.x, cell.y);
 
-const findCell = (matrix, x, y) => !matrix[y] || !matrix[y][x] ? false : matrix[y][x];
+  for (const cell of cells) cell.mineCount++;
+};
+
+const findCell = (matrix, x, y) => (!matrix[y] || !matrix[y][x] ? false : matrix[y][x]);
 
 const aroundCells = (matrix, x, y) => {
   const cells = [];
-  for (let dx = -1; dx <= 1; dx++){
-    for (let dy = -1; dy <= 1; dy++){
+  for (let dx = -1; dx <= 1; dx++) {
+    for (let dy = -1; dy <= 1; dy++) {
       if (dx === 0 && dy === 0) continue;
       const cell = findCell(matrix, x + dx, y + dy);
       if (cell) cells.push(cell);
     }
   }
   return cells;
-}
+};
 
 const cellById = (matrix, id) => {
-  for (let y = 0;y < matrix.length; y++){
-    for (let x = 0; x < matrix[y].length; x++){
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
       if (matrix[y][x].id === id) return matrix[y][x];
     }
   }
   return false;
-}
+};
 
 const matrixHTML = matrix => {
   const gameDiv = document.createElement('div');
@@ -77,7 +77,7 @@ const matrixHTML = matrix => {
   for (let y = 0; y < matrix.length; y++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
-    for (let x = 0; x < matrix[y].length; x++){
+    for (let x = 0; x < matrix[y].length; x++) {
       const cell = matrix[y][x];
       const imgDiv = document.createElement('img');
       imgDiv.draggable = false;
@@ -92,32 +92,32 @@ const matrixHTML = matrix => {
     }
     gameDiv.append(rowDiv);
   }
-  
+
   return gameDiv;
-}
+};
 
 const forEach = (matrix, handler) => {
   for (let y = 0; y < matrix.length; y++) {
-    for (let x = 0; x < matrix[y].length; x++){
+    for (let x = 0; x < matrix[y].length; x++) {
       handler(matrix[y][x]);
     }
   }
-}
+};
 
 const openSpread = (matrix, x, y) => {
   const cell = findCell(matrix, x, y);
   if (cell.mineCount || cell.mine || cell.flag) return;
-  
+
   forEach(matrix, x => x._mark = false);
-  
+
   cell._mark = true;
   let flag = true;
-  while(flag){
+  while (flag) {
     flag = false;
     for (let y = 0; y < matrix.length; y++) {
-      for (let x = 0; x < matrix[y].length; x++){
+      for (let x = 0; x < matrix[y].length; x++) {
         const cell = matrix[y][x];
-        if(!cell._mark || cell.mineCount) continue;
+        if (!cell._mark || cell.mineCount) continue;
         const cells = aroundCells(matrix, x, y);
         for (const cell of cells) {
           if (cell._mark) continue;
@@ -130,13 +130,13 @@ const openSpread = (matrix, x, y) => {
     }
   }
   forEach(matrix, x => {
-      if (x._mark) x.visible = true;
-      delete x._mark
-    });
+    if (x._mark) x.visible = true;
+    delete x._mark;
+  });
 
-}
+};
 
-const win = (matrix) => {
+const win = matrix => {
   const flags = [];
   const mines = [];
   forEach(matrix, cell => {
@@ -149,21 +149,21 @@ const win = (matrix) => {
   }
 
   for (let y = 0; y < matrix.length; y++) {
-    for (let x = 0; x < matrix[y].length; x++){
+    for (let x = 0; x < matrix[y].length; x++) {
       const cell = matrix[y][x];
       if (!cell.mine && !cell.visible) return false;
     }
   }
 
   return true;
-}
+};
 
-const lose = (matrix) => {
+const lose = matrix => {
   for (let y = 0; y < matrix.length; y++) {
-    for (let x = 0; x < matrix[y].length; x++){
+    for (let x = 0; x < matrix[y].length; x++) {
       const cell = matrix[y][x];
       if (cell.mine && cell.visible) return true;
     }
   }
   return false;
-}
+};
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+'use strict';
+
 let matrix = null;
 let running = false;
 
 document
   .querySelector('button')
-  .addEventListener('click', () => init(10, 10, 10))
+  .addEventListener('click', () => init(10, 10, 10));
 
 
 const init = (col, row, mines) => {
@@ -11,24 +13,23 @@ const init = (col, row, mines) => {
   running = true;
   for (let i = 0; i < mines; i++) {
     randomMine(matrix);
-  };
+  }
 
   updateData();
-}
+};
 
 const updateData = () => {
   if (!running) return;
 
   if (lose(matrix)) {
     forEach(matrix, x => {
-      if(x.mine){
+      if (x.mine) {
         x.visible = true;
-      } 
-    })
+      }
+    });
     alert('LOSE');
     running = false;
-  }
-  else if (win(matrix)){
+  } else if (win(matrix)) {
     alert('WIN');
     running = false;
   }
@@ -36,20 +37,20 @@ const updateData = () => {
   const gameDiv = matrixHTML(matrix);
   const appDiv = document.querySelector('#app');
   appDiv.innerHTML = '';
-  appDiv.append(gameDiv); 
+  appDiv.append(gameDiv);
   appDiv
     .querySelectorAll('img')
     .forEach(img => {
       img.addEventListener('mousedown', mousedownHandler);
       img.addEventListener('mouseup', mouseupHandler);
       img.addEventListener('mouseleave', mouseleaveHandler);
-    })
-}
+    });
+};
 
 
 
-function mousedownHandler (event) {
-  const {cell, left, right} = getInfo(event);
+function mousedownHandler(event) {
+  const { cell, left, right } = getInfo(event);
   if (left) cell.left = true;
   if (right) cell.right = true;
   if (cell.right && cell.left) {
@@ -57,25 +58,25 @@ function mousedownHandler (event) {
   }
 
   updateData();
-};
+}
 
 
-function mouseupHandler (event) {
-  const {cell, left, right} = getInfo(event);
+function mouseupHandler(event) {
+  const { cell, left, right } = getInfo(event);
   const both = cell.right && cell.left && (left || right);
   const leftClick = !both && cell.left && left;
   const rightClick = !both && cell.right && right;
-  
-  if (both) forEach(matrix, x => x.poten = false)
+
+  if (both) forEach(matrix, x => x.poten = false);
   if (left) cell.left = false;
   if (right) cell.right = false;
   if (leftClick) leftHandler(cell);
   else if (rightClick) rightHandler(cell);
 
   updateData();
-};
+}
 
-function mouseleaveHandler (event) {
+function mouseleaveHandler(event) {
   const info = getInfo(event);
   info.cell.left = false;
   info.cell.rigth = false;
@@ -89,36 +90,36 @@ const getInfo = event => {
     left: event.which === 1,
     right: event.which === 3,
     cell: cellById(matrix, parseInt(id)),
-  }
-}
+  };
+};
 
-function leftHandler (cell) {
+function leftHandler(cell) {
   if (cell.visible || cell.flag) return;
   cell.visible = true;
 
   if (!cell.mine && !cell.mineCount) {
     openSpread(matrix, cell.x, cell.y);
   }
-};
+}
 
-function rightHandler (cell) {
+function rightHandler(cell) {
   if (!cell.visible) cell.flag = !cell.flag;
 
-};
+}
 
-function bothHandler (cell) {
+function bothHandler(cell) {
   if (!cell.visible || !cell.mineCount) return;
   const cells = aroundCells(matrix, cell.x, cell.y);
   const flags = cells.filter(x => x.flag).length;
-  
+
   if (flags === cell.mineCount)
     cells
       .filter(x => !x.flag && !x.visible)
-      .forEach(cell => { 
-          cell.visible = true;
-          openSpread(matrix, cell.x, cell.y);
-        })
-  else cells.filter(x => !x.flag && !x.visible).forEach(cell => cell.poten = true)
-};
+      .forEach(cell => {
+        cell.visible = true;
+        openSpread(matrix, cell.x, cell.y);
+      });
+  else cells.filter(x => !x.flag && !x.visible).forEach(cell => cell.poten = true);
+}
 
 init(10, 10, 10);
